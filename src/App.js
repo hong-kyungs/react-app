97edc3d711b0763f9de97df665729b0f81fcdab7
hong-kyungs <hkyungs11@naver.com>
2023-08-15 23:47:41 +0900

    react - 컴포넌트 만들기

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<header>
+				<h1>WEB</h1>
+			</header>
+			<nav>
+				<ol>
+					<li>
+						<a href='/read/1'>html</a>
+					</li>
+					<li>
+						<a href='/read/2'>css</a>
+					</li>
+					<li>
+						<a href='/read/3'>javaScript</a>
+					</li>
+				</ol>
+			</nav>
+			<article>
+				<h2>Welcome</h2>
+				Hello, Web
+			</article>
+		</div>
+	);
+}
+
+export default App;
