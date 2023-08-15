24904bb97b57c4b98548c665f47e644edc5f94a4
hong-kyungs <hkyungs11@naver.com>
2023-08-16 00:15:39 +0900

    react - 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import './App.css';
+function Header() {
+	return (
+		<header>
+			<h1>
+				<a href='/'>WEB</a>
+			</h1>
+		</header>
+	);
+}
+function Nav() {
+	return (
+		<nav>
+			<ol>
+				<li>
+					<a href='/read/1'>html</a>
+				</li>
+				<li>
+					<a href='/read/2'>css</a>
+				</li>
+				<li>
+					<a href='/read/3'>javaScript</a>
+				</li>
+			</ol>
+		</nav>
+	);
+}
+
+function Article() {
+	return (
+		<article>
+			<h2>Welcome</h2>
+			Hello,web
+		</article>
+	);
+}
 
 function App() {
 	return (
 		<div>
-			<header>
-				<h1>WEB</h1>
-			</header>
-			<nav>
-				<ol>
-					<li>
-						<a href='/read/1'>html</a>
-					</li>
-					<li>
-						<a href='/read/2'>css</a>
-					</li>
-					<li>
-						<a href='/read/3'>javaScript</a>
-					</li>
-				</ol>
-			</nav>
-			<article>
-				<h2>Welcome</h2>
-				Hello, Web
-			</article>
+			<Header></Header>
+			<Nav></Nav>
+			<Article></Article>
 		</div>
 	);
 }
